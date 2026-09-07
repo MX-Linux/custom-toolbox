dc915949d1398a81e408ecef2018460455f985f0
Adrian <adrian@mxlinux.org>
2026-09-06 22:39:21 -0400

    Add safety check for DISPLAY

--- src/main.cpp
+++ src/main.cpp
@@ -78,6 +78,13 @@ static QString promptForListFile()
 
 int main(int argc, char *argv[])
 {
+    if (qEnvironmentVariableIsEmpty("DISPLAY") && qEnvironmentVariableIsEmpty("WAYLAND_DISPLAY")
+        && qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
+        qWarning("custom-toolbox: no display available (DISPLAY and WAYLAND_DISPLAY are both unset); "
+                "a graphical session is required to run this program.");
+        return EXIT_FAILURE;
+    }
+
     if (getuid() == 0) {
         qputenv("XDG_RUNTIME_DIR", "/run/user/0");
         qunsetenv("SESSION_MANAGER");
